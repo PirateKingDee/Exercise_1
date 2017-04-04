9a7b19079ccc747614739d10bf90b901c261eb7f
PirateKingDee <andyliang987@hotmail.com>
2017-04-04 11:28:14 -0700

    EchoServer and EchoClient working.

--- EchoClient.java
+++ EchoClient.java
@@ -4,14 +4,40 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.Socket;
 
-public final class EchoClient {
+import java.io.OutputStream;
+import java.io.PrintStream;
 
+public final class EchoClient {
+    
     public static void main(String[] args) throws Exception {
+        //Connect to Server
         try (Socket socket = new Socket("localhost", 22222)) {
+            //Set up input stream to get message from server
             InputStream is = socket.getInputStream();
             InputStreamReader isr = new InputStreamReader(is, "UTF-8");
             BufferedReader br = new BufferedReader(isr);
             System.out.println(br.readLine());
+            
+            
+            //while its connected to server
+            while(true){
+                System.out.print("Client > ");
+                //Buffer reader that reads user's input
+                BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+                //set up output stream to send message to server
+                PrintStream toServer = new PrintStream(socket.getOutputStream(), true, "UTF-8");
+                String str = userInput.readLine();
+                //send the user message to server
+                toServer.printf("%s%n",str);
+                //read the message send from server
+                String fromServer = br.readLine();
+                //if message is "exit", close socket and end while loop
+                if(fromServer.equals("exit")){
+                    socket.close();
+                    break;
+                }
+                System.out.println("Server > "+fromServer);
+            }
         }
     }
 }
--- EchoServer.java
+++ EchoServer.java
@@ -4,19 +4,55 @@ import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public final class EchoServer {
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
 
+public final class EchoServer {
+    
     public static void main(String[] args) throws Exception {
+        
+        //setup server
         try (ServerSocket serverSocket = new ServerSocket(22222)) {
             while (true) {
-                try (Socket socket = serverSocket.accept()) {
-                    String address = socket.getInetAddress().getHostAddress();
-                    System.out.printf("Client connected: %s%n", address);
-                    OutputStream os = socket.getOutputStream();
-                    PrintStream out = new PrintStream(os, true, "UTF-8");
-                    out.printf("Hi %s, thanks for connecting!%n", address);
+                try {
+                    //accept client connect to this server
+                    Socket socket = serverSocket.accept();
+
+                        try{
+                            String address = socket.getInetAddress().getHostAddress();
+                            System.out.printf("Client connected: %s%n", address);
+                            //out stream message to client
+                            OutputStream os = socket.getOutputStream();
+                            PrintStream out = new PrintStream(os, true, "UTF-8");
+                            out.printf("Hi %s, thanks for connecting!%n", address);
+                            //while client is connected, read the message sent from client, and send the message back to client.
+                            //If message is exit, break the while loop and display disconnected messge
+                            while(true){
+                                //buffer reader to read message from client
+                                BufferedReader fromClient = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
+                                String message = fromClient.readLine();
+                                if(message.equals("exit")){
+                                    out.printf("%s%n",message);
+                                    System.out.printf("Client disconnected: %s%n", address);
+                                    break;
+                                }
+                                else out.printf("%s%n",message);
+                            }
+                        }
+                        catch(Exception e){
+                            
+                        }
+                    
                 }
+                catch(Exception e){
+                    
+                }
+                
             }
         }
+        catch(Exception e){
+            
+        }
     }
+    
 }
